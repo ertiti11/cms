c70fbe88eb8f5a199ef6f94b113fd5c8edc64319
DonRathor <futoilo@gmail.com>
2024-04-29 23:07:50 +0200

    Aniadidas rutas en web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\SettingController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LogController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,4 +23,12 @@ Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
 
+Route::resource('/user', UserController::Class);
+Route::resource('/setting', SettingController::Class);
+Route::resource('/post', PostController::Class);
+Route::resource('/log', LogController::Class);
+Route::resource('/comment', CommentController::Class);
+Route::resource('/category', CategoryController::Class);
+
 require __DIR__.'/auth.php';
+
