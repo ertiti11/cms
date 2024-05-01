e9e1bec03c2ae6f607160a4ecfaced3b9ec2de5e
DonRathor <futoilo@gmail.com>
2024-05-01 23:32:57 +0200

    agregadas rutas seguras usuario en api.php

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SchemaController;
 use App\Http\Controllers\GenericCollectionController;
+use App\Http\Controllers\Auth\RegisteredUserController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
@@ -27,3 +28,13 @@ Route::patch('/collections/{collection}/records/{id}', [GenericCollectionControl
 //delete
 Route::post('/collections/{collection}/records/{id}', [GenericCollectionController::class, 'list']);
 
+//list/search
+Route::get('/collections/users/records', [RegisteredUserController::class, 'view']);
+//view
+Route::get('/collections/users/records/{id}', [RegisteredUserController::class, 'read']);
+//create
+Route::post('/collections/users/records', [RegisteredUserController::class, 'create']);
+//update
+Route::patch('/collections/users/records/{id}', [RegisteredUserController::class, 'delete']);
+//delete
+Route::post('/collections/users/records/{id}', [RegisteredUserController::class, 'list']);
