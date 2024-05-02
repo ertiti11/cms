8cd7f64fdc4862d0be429c6c06be5caeeea7d55b
DonRathor <futoilo@gmail.com>
2024-05-02 02:47:29 +0200

    Creado endpoint /collections/all y comprobada su funcionalidad

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\SchemaController;
 use App\Http\Controllers\GenericCollectionController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 
+
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
@@ -31,5 +32,6 @@ Route::patch('/collections/{collection}/records/{id}', [GenericCollectionControl
 //delete
 Route::post('/collections/{collection}/records/{id}', [GenericCollectionController::class, 'list']);
 
-
+//Obtener todas las tablas
+Route::get('/collections/all', [SchemaController::class, 'view']);
 
