5f952387c34f1a2c29e218318ecc72db5096ecd5
DonRathor <futoilo@gmail.com>
2024-05-26 20:46:04 +0200

    Arreglado MainBody.jsx que hacía fallar intentando hacer un import que no se usaba ni existía

--- ui/src/components/MainBody.jsx
+++ ui/src/components/MainBody.jsx
@@ -1,7 +1,6 @@
 
 import React from 'react';
 import Title from './MainBodyComponents/Title';
-import Search from './MainBodyComponents/Search';
 import TableComponent from './MainBodyComponents/TableComponent';
 
 class MainBody extends React.Component {
