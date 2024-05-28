8a2345d6a7053c0ebda806562cda3a008a702d63
Diego <diegoalarcondrum@gmail.com>
2024-05-28 22:47:28 +0200

    commitToPull

--- ui/src/components/MainBody.jsx
+++ ui/src/components/MainBody.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import Title from './MainBodyComponents/Title';
-import Search from './MainBodyComponents/Search';
+/* import Search from './MainBodyComponents/Search'; */
 import TableComponent from './MainBodyComponents/TableComponent';
 
 class MainBody extends React.Component {
