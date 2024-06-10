d67e94ec614eeb308d4f55552dbdaaa0c815b8f6
DonRathor <futoilo@gmail.com>
2024-06-10 23:27:53 +0200

    comentarios aniadidos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use App\Models\Post;
 use App\Models\Category;
 use App\Models\Blog;
+use App\Models\Comment;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -18,11 +19,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory(10)->create();
-
         Category::factory(10)->create();
         Post::factory(10)->create();
-
         Blog::factory(10)->create();
-
+        Comment::factory(10)->create();
     }
 }
