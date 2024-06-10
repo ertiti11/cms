806a422873f475ca91d09baaae58e6d6dc156649
DonRathor <futoilo@gmail.com>
2024-06-10 19:38:29 +0200

    Guardado de imagenes funciona

--- app/Http/Controllers/GenericCollectionController.php
+++ app/Http/Controllers/GenericCollectionController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 
 class GenericCollectionController extends Controller 
 {
@@ -13,6 +14,16 @@ class GenericCollectionController extends Controller
     public function view($collection)
     {
         $data = DB::table($collection)->get();
+
+        // Mapear los datos para convertir las rutas de las imágenes
+        $data = $data->map(function ($item) {
+            if (isset($item->thumbnail)) {
+                // Convertir la ruta relativa en una URL completa
+                $item->thumbnail_url = Storage::url($item->thumbnail);
+            }
+            return $item;
+        });
+
         return response()->json($data);
     }
 
@@ -24,17 +35,38 @@ class GenericCollectionController extends Controller
 
     public function create(Request $request, $collection)
     {
-        $data = $request->all();
-        $validator = Validator::make($data, [
-            'data' => 'required|array',
-        ]);
+        if ($collection == 'posts') {
+            $data = $request->all();
 
-        if ($validator->fails()) {
-            return response()->json(['error' => $validator->errors()], 400);
+            if ($request->hasFile('data.thumbnail')) {
+                $image = $request->file('data.thumbnail');
+                $path = $image->store('images', 'public');
+                $data['data']['thumbnail'] = $path; // Guardar la ruta en los datos
+            }
+
+            $validator = Validator::make($data, [
+                'data' => 'required|array',
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json(['error' => $validator->errors()], 400);
+            }
+
+            $id = DB::table($collection)->insertGetId($data['data']);
+            return response()->json(['id' => $id]);
+        } else {
+            $data = $request->all();
+            $validator = Validator::make($data, [
+                'data' => 'required|array',
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json(['error' => $validator->errors()], 400);
+            }
+
+            $id = DB::table($collection)->insertGetId($data['data']);
+            return response()->json(['id' => $id]);
         }
-
-        $id = DB::table($collection)->insertGetId($data['data']);
-        return response()->json(['id' => $id]);
     }
 
     public function update(Request $request, $collection, $id)
